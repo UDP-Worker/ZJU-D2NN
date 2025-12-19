e217262359944f34fce1af09993793f093dcf04a
leaf-master <leaf_master@protonmail.ch>
2025-12-19 09:50:00 +0800

    Add description os training process

--- train_system.py
+++ train_system.py
@@ -2,5 +2,7 @@
 Train a neural network or use a normal function to describe the propagating process
 from SLM1 to SLM2. Also train a neural network or use a normal function to describe
 the propagating process from SLM2 to camera.
+
+To train these two neural network, we can utilize programmable SLM2.
 """
 
